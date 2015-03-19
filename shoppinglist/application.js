821ea7a56946eb86bc50be86b6e9bfcfde04574b
bacevedo1 <bacevedo1@kippnyccp.org>
2015-03-19 09:01:30 -0400

    Update application.js

--- shoppinglist/application.js
+++ shoppinglist/application.js
@@ -1,5 +1,7 @@
-$('#button').on('click',function(){
+$(document).ready(function(){
+  $('#button').on('click',function(){
   var item = $('#placeholder').val();
   $('ul').append('<li>'+item+'</li>');
+  });
 });
-$('#button').on('click',function)
+
