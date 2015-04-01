135794a527bf37d39c8d304a96db546509d36b22
bacevedo1 <bacevedo1@kippnyccp.org>
2015-04-01 08:41:44 -0400

    Update application.js

--- guess_my_number/application.js
+++ guess_my_number/application.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
   //find a way for the program to choose a random number between 1 and 100, save this as a variable
   var comp_integer = Math.floor(Math.random()*100);
@@ -11,16 +10,14 @@ $(document).ready(function(){
     
     //and compare this guess to the random number that the computer picked
     //if the user guessed the correct number...
-    
-      //what happens if the guess is correct?
-      
-    //if the user guessed too high...
-    
-      //update the 'feedback' paragraph to tell them to guess lower
-      
-    //otherwise, the user guessed too low...
-    
-      //update the 'feedback' paragraph to tell them to guess higher
-    
+    if (play_guess === comp_integer){
+      console.log("You're Correct!"); //what happens if the guess is correct
+    }
+    else if (play_guess > comp_integer){ //if the user guessed too high...
+      console.log("Guess Lower!");  //update the 'feedback' paragraph to tell them to guess lower 
+    }
+    else {
+      console.log("Guess Higher!"); //update the 'feedback' paragraph to tell them to guess higher
+    }
   });
 });
