7edf21b650cebe4221cb151b9ea074b56725083c
bacevedo1 <bacevedo1@kippnyccp.org>
2015-04-15 09:38:55 -0400

    Update application.js

--- guess_my_number/application.js
+++ guess_my_number/application.js
@@ -1,31 +1,24 @@
 $(document).ready(function(){
-  var guess = 0;
-  //find a way for the program to choose a random number between 1 and 100, save this as a variable
-  var comp_integer = Math.floor(Math.random()*100);
+  var guess = 0; // starts program off by setting the guess to 0 
+  var comp_integer = Math.floor(Math.random()*100); //program chooses random number from 0 to 100
   console.log(comp_integer);
-  //when the player clicks on the 'guess' button
-  $('button').on('click', function(){
+    $('button').on('click', function(){ //when the player clicks on the 'guess' button
     $("#feedback").empty();
-    //save their guess as a variable
     guess = guess + 1;
     var play_guess = parseInt($('input').val());
     console.log(play_guess);
     //and compare this guess to the random number that the computer picked
-    //if the user guessed the correct number...
     $("#guess_counter").empty();
     $("#guess_counter").append(guess);// we tried to update it but it didnt work...sorry
-    if(play_guess===comp_integer){
-      //what happens if the guess is correct?
+    if(play_guess===comp_integer){    //if the user guessed the correct number...
       $("#feedback").append("Correct!");
     }
-    //if the user guessed too high...
-    else if(play_guess>comp_integer){
+    else if(play_guess>comp_integer){ //if the user guessed too high...
  $("#feedback").append("number too high!");
     }
     else
       //update the 'feedback' paragraph to tell them to guess lower
-      
-    //otherwis$("#feedback").append("number too high!");e, the user guessed too low...
+    //if the user guessed too low...
     $("#feedback").append("number too low!");
       //update the 'feedback' paragraph to tell them to guess higher
     $("#guess_counter").empty();
