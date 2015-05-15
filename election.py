56a79f65cb90c80fb2c3dce98fe6ee052e5d34ed
bacevedo1 <bacevedo1@kippnyccp.org>
2015-05-15 09:07:12 -0400

    Update election.py

--- election.py
+++ election.py
@@ -1,8 +1,8 @@
-from random import random
+from random import random #choose random number 
 total_wins = 0
-for i in range(10000):
+for i in range(10000): #repeats it 10,000 times
   num = random()
-  region_wins = 0
+  region_wins = 0 #sets region wins to 0 
   region1 = .87
   region2 = .65
   region3 = .17
@@ -14,4 +14,4 @@ for i in range(10000):
     region_wins = region_wins + 1
   if region_wins >= 2:
     total_wins = total_wins + 1
-percentage = total_wins / 10000
+percentage = total_wins / 10000 #turns it into a percentage
