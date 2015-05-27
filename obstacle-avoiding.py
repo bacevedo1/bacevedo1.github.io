e21197dc3f89cff7f5fdb6ffc2a6bc5596859e88
bacevedo1 <bacevedo1@kippnyccp.org>
2015-05-27 09:32:32 -0400

    Update obstacle-avoiding.py

--- obstacle-avoiding.py
+++ obstacle-avoiding.py
@@ -1,5 +1,14 @@
 from gopigo import *
 set_speed(100)
+led_on()
+def move_left():
+  enc_tgt(0,1,14)
+  time.sleep(.1)
+  left() 
+  time.sleep(4)
 while us_dist(15) > 25:
   fwd()
 stop()
+move_left()
+while us_dist(15) > 25:
+  fwd()
